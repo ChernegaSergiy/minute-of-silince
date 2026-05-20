e61157faa8c600c05ac33e52a547a1ea2f618b64
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-05-20 15:54:04 +0300

    feat(platform): Add macOS platform implementation

---
+++ src-tauri/src/platform/macos/mod.rs
@@ -0,0 +1,26 @@
+pub mod media;
+pub mod volume;
+
+pub struct MacosPlatform;
+
+#[async_trait::async_trait]
+impl super::Platform for MacosPlatform {
+    fn get_volume(&self) -> crate::error::Result<u8> {
+        self::volume::get_volume()
+    }
+    fn set_volume(&self, level: u8) -> crate::error::Result<()> {
+        self::volume::set_volume(level)
+    }
+    fn is_muted(&self) -> crate::error::Result<bool> {
+        self::volume::is_muted()
+    }
+    fn set_mute(&self, mute: bool) -> crate::error::Result<()> {
+        self::volume::set_mute(mute)
+    }
+    async fn pause_media(&self) -> crate::error::Result<Vec<String>> {
+        self::media::pause_all().await
+    }
+    async fn resume_media(&self, players: Vec<String>) -> crate::error::Result<()> {
+        self::media::resume_specific(players).await
+    }
+}
